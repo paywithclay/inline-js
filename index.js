d5f205e46a4a1e87e1713dcb3c7db480f4429ca7
CeliaMini <afulafavour@gmail.com>
2024-10-12 02:38:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,11 @@ class Clay {
 
   showLoading() {
     const loading = document.createElement("div");
-    loading.className = "loading";
-    loading.innerText = "Loading...";
+    loading.className = "lds-ripple";
+    loading.innerHTML = `
+      <div></div>
+      <div></div>
+    `;
     document.body.appendChild(loading);
 
     // Show shadow covering entire page
@@ -57,7 +60,7 @@ class Clay {
   }
 
   removeLoading() {
-    const loading = document.querySelector(".loading");
+    const loading = document.querySelector(".lds-ripple");
     const shadow = document.querySelector(".shadow");
     if (loading) loading.remove(); // Remove loading indicator
     if (shadow) shadow.remove(); // Remove shadow
@@ -99,13 +102,59 @@ class Clay {
         text-decoration: none;
         cursor: pointer;
       }
-      .loading {
+      .lds-ripple,
+      .lds-ripple div {
+        box-sizing: border-box;
+      }
+      .lds-ripple {
+        display: inline-block;
         position: fixed;
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
-        font-size: 20px;
+        width: 80px;
+        height: 80px;
         z-index: 1002; /* Above shadow */
+      }
+      .lds-ripple div {
+        position: absolute;
+        border: 4px solid currentColor;
+        opacity: 1;
+        border-radius: 50%;
+        animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+      }
+      .lds-ripple div:nth-child(2) {
+        animation-delay: -0.5s;
+      }
+      @keyframes lds-ripple {
+        0% {
+          top: 36px;
+          left: 36px;
+          width: 8px;
+          height: 8px;
+          opacity: 0;
+        }
+        4.9% {
+          top: 36px;
+          left: 36px;
+          width: 8px;
+          height: 8px;
+          opacity: 0;
+        }
+        5% {
+          top: 36px;
+          left: 36px;
+          width: 8px;
+          height: 8px;
+          opacity: 1;
+        }
+        100% {
+          top: 0;
+          left: 0;
+          width: 80px;
+          height: 80px;
+          opacity: 0;
+        }
       }
       .shadow {
         position: fixed;
